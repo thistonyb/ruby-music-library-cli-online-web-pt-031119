6a5e4e61294fdd93e673882c5b525ef8afe73e43
Tony Baker <this.tonybaker@gmail.com>
2019-04-18 04:09:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,5 +56,19 @@ class MusicLibraryController
       end
     end
 
+      def list_songs_by_artist
+        counter = 1
+        puts "Please enter the name of an artist:"
+        user_imput = gets
+        if artist = Artist.find_by_name(user_imput)
+          artist.songs.sort{|a, z| a.name <=> z.name}.each do |song|
+            puts "#{counter}. #{song.name} - #{song.genre.name}"
+            counter += 1
+          end
+        end
+      end
+      
+    end
+
 
 end
