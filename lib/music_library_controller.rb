477834e2adbe46d2a02b80abf631ad62c63c70c7
Tony Baker <this.tonybaker@gmail.com>
2019-04-18 04:46:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -27,8 +27,9 @@ class MusicLibraryController
       puts "#{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       counter += 1
     end
+    binding.pry
   end
-  binding.pry
+  
 
   def list_artists
     counter = 1
