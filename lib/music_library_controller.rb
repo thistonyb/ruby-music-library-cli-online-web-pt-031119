ad195f3930430cbc543858367a0214eea33193d7
Tony Baker <this.tonybaker@gmail.com>
2019-04-18 04:13:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -48,12 +48,12 @@ class MusicLibraryController
       counter = 1
       puts "Please enter the name of an artist:"
       user_imput = gets
-      # if artist = Artist.find_by_name(user_imput)
-      #   artist.songs.sort{|a, z| a.name <=> z.name}.each do |song|
-      #     puts "#{counter}. #{song.name} - #{song.genre.name}"
-      #     counter += 1
-      #   end
-      # end
+      if artist = Artist.find_by_name(user_imput)
+        artist.songs.sort{|a, z| a.name <=> z.name}.each do |song|
+          puts "#{counter}. #{song.name} - #{song.genre.name}"
+          counter += 1
+        end
+      end
     end
 
       def list_songs_by_genre
